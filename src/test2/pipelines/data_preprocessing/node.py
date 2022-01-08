8266b2e1800ffdbef842ca6f68c02c545ea2362a
Daniel Falk <daniel.falk.1@fixedit.ai>
2022-01-08 20:19:19 +0100

    Add DVC pipelines to download and preprocess video
    ..and change some filepaths and indexes to work with the new video.

--- src/test2/pipelines/data_preprocessing/node.py
+++ src/test2/pipelines/data_preprocessing/node.py
@@ -1,23 +1,23 @@
 def video_to_frames(video):
-    if len(video) != 30:
-        raise ValueError("Node expects video with exactly 30 frames")
+    if len(video) != 24:
+        raise ValueError("Node expects video with exactly 24 frames")
 
     # Test forward stepping in video
-    imgs = {"%08d" % i: video[i] for i in range(5)}
+    imgs = {"%08d" % i: video[i] for i in range(4)}
     # Test backward stepping
-    imgs.update({"%08d" % i: video[i] for i in reversed(range(5, 10))})
+    imgs.update({"%08d" % i: video[i] for i in reversed(range(4, 8))})
     # Test individual indexes
-    for i in [12,14,11,13,10]:
+    for i in [10,11,9,8]:
         imgs["%08d" % i] = video[i]
     # Test slices:
-    for offset, img in enumerate(video[15:20]):
-        imgs["%08d" % (15 + offset)] = img
+    for offset, img in enumerate(video[12:16]):
+        imgs["%08d" % (12 + offset)] = img
     # Test slices of slices and negative slices
-    for offset, img in enumerate(video[-10:-5]):
-        imgs["%08d" % (20 + offset)] = img
+    for offset, img in enumerate(video[-8:-4]):
+        imgs["%08d" % (16 + offset)] = img
     # Test slices
-    for offset, img in enumerate(video[-5:-1]):
-        imgs["%08d" % (25 + offset)] = img
+    for offset, img in enumerate(video[-4:-1]):
+        imgs["%08d" % (20 + offset)] = img
     # Test negative indexing
-    imgs["00000030"] = video[-1]
+    imgs["00000024"] = video[-1]
     return imgs
